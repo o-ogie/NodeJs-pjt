5dd9339d17483353fb27859d52362b4f52119068
jungmin <justin930522@gmail.com>
2022-12-26 15:26:43 +0900

    feat:boardRoute.js 생성 및 기본세팅

---
+++ routes/board/boardRoute.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = require('router')
+const boardController = require('')
+
+router.get('/list', boardController.);
+
+router.get('/view', boardController.);
+
+router.get('/write', boardController.);
+
+router.post('/write', boardController.);
+
+router.get('/modify', boardController. boardController.);
+
+router.post('/modify', boardController.);
+
+router.get(
+    '/delete',
+    boardController.,
+    boardController.
+);
+
+
+module.exports = router;
