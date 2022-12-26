f141b04ee4a8a5ce8a65083b38c6ef7472008458
o-ogie <wrld.worthy@gmail.com>
2022-12-26 10:00:15 +0900

    최초 커밋

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+
+const user = require('./user.route')
+const board = require('./board.route')
+const admin = require('./admin')
+
+router.use('/user', user)
+router.use('/board', board)
+router.use('/admin', admin)
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const cookieParser = require('cookie-parser')
+const router = require('./routes')
+
+const app = express()
+
+app.set('veiw engine', 'html')
+nunjucks.configure('views', {
+    express : app
+})
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:false}))
+app.use(cookieParser())
+app.use(router)
+
+
+app.listen(3000,()=>{
+    console.log('Server Start')
+})
