52ee29306d869cbe31ad2619ad639f53351c64e1
jungmin <justin930522@gmail.com>
2022-12-26 16:54:13 +0900

    feat:route,controller연동,remove:board/index.html삭제

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-const user = require('./user.route')
-const board = require('./board.route')
-const admin = require('./admin')
+// const user = require('./board/')
+const board = require('./board/boardRoute')
+// const admin = require('./admin')
 
-router.use('/user', user)
+// router.use('/user', user)
 router.use('/board', board)
-router.use('/admin', admin)
+// router.use('/admin', admin)
 
 module.exports = router
