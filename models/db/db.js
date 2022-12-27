3e2515e8b0bb3e825566058bb9624f3f2d5f5e8c
jungmin <justin930522@gmail.com>
2022-12-27 09:25:26 +0900

    feat:예비db파일세팅

--- models/db/db.js
+++ models/db/db.js
@@ -1,2 +1,11 @@
 const mysql = require('mysql2')
-const pool = mysql.createPool
+const pool = mysql.createPool({
+    host:'127.0.0.1',
+    port:'3306',
+    user:'jung',
+    password:'jung',
+    database:'jung',
+    connectionLimit:5,
+}).promise()
+
+module.exports = pool
