2649e7ea3fe0cb37b6c5a44dd70eb8418a7edb2e
o-ogie <wrld.worthy@gmail.com>
2022-12-28 18:33:10 +0900

    fix: 브런치 스위치

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const user = require('./user.route')
-const board = require('./board.route')
+const board = require('./board/boardRoute')
 const admin = require('./admin.route')
 
 
