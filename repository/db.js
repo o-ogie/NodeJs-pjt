018263da4cdaa41a5036c426887e37ef3f93f4c2
hongtae <ambious12@gmail.com>
2022-12-29 14:09:39 +0900

    feat: join -> 환영페이지 이동완료

--- repository/db.js
+++ repository/db.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host : "127.0.0.1",
     port : "3306",
     user : "root",
-    password : " ",
+    password : "tiajsrlf1!",
     database : "node",
     connectionLimit : 5,
     dateStrings : String
