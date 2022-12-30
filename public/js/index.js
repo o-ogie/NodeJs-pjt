3302a6d797177247e2a983e118d2a4b09a2109c8
o-ogie <wrld.worthy@gmail.com>
2022-12-30 13:48:47 +0900

    fix:슬라이드 끊김, 자동 구현오류 해결

--- public/js/index.js
+++ public/js/index.js
@@ -1,80 +1,93 @@
-const kindWrap =  document.querySelector('.kind_wrap')
-const slider = document.querySelector('.slider')
-const slideLis = document.querySelectorAll('li')
-const moveButton = document.querySelector('.arrow')
+const   slides = document.querySelector('#slides')
+        slide = document.querySelectorAll('#slides > li')
+        prevBtn = document.querySelector('#prev')
+        nextBtn = document.querySelector('#next')
+
+let currentIndex = 0
+    slideCount = slide.length
+    slideWidth = 1200
+    slideMargin = 30
+    timer = undefined
 
 
-const clone1 = slideLis[0].cloneNode(true)
-const cloneLast = slideLis[slideLis.length - 1].cloneNode(true)
-slider.insertBefore(cloneLast, slideLis[0])
-slider.appendChild(clone1)
 
+const makeClone = () => {
 
-let currentIdx = 0
-let translate = 0
-const speedTime = 500
-
-
-const sliderCloneLis = slider.querySelectorAll('li')
-const liWidth = slideLis[0].clientWidth
-const sliderWidth = liWidth * sliderCloneLis.length
-slider.style.width = `${sliderWidth}px`
-currentIdx = 1
-translate = -liWidth
-slider.style.transform = `translateX(${translate}px)`
-
-
-moveButton.addEventListener('click', moveSlide)
-
-
-function move(D) {
-    currentIdx += (-1 * D)
-    translate += liWidth * D
-    slider.style.transform = `translateX(${translate}px)`
-    slider.style.transition = `all ${speedTime}ms ease`
-}
-
-
-function moveSlide(event) {
-    event.preventDefault()
-    if (event.target.className === 'next') {
-    move(-1)
-    if (currentIdx === sliderCloneLis.length -1)
-        setTimeout(() => {
-        slider.style.transition = 'none'
-        currentIdx = 1
-        translate = -liWidth
-        slider.style.transform = `translateX(${translate}px)`
-        }, speedTime)
-    } else {
-        move(1)
-        if (currentIdx === 0) {
-        setTimeout(() => {
-            slider.style.transition = 'none'
-            currentIdx = sliderCloneLis.length -2
-            translate = -(liWidth * currentIdx)
-            slider.style.transform = `translateX(${translate}px)`
-        }, speedTime)
-        }
-    }
-}
-
-function sliding() {
-    move(-1)
-    if (currentIdx === sliderCloneLis.length -1)
-        setTimeout(() => {
-            slider.style.transition = 'none'
-            currentIdx = 1
-            translate = -liWidth
-            slider.style.transform = `translateX(${translate}px)`
-        }, speedTime)
+    for ( let i =0; i<slideCount; i++){
+        const cloneSlide = slide[0].cloneNode(true)
+        cloneSlide.classList.add = 'clone'
+        slides.appendChild(cloneSlide)
     }
 
-function showSliding() {
-    setInterval(sliding, 2000)
+    for (let i=slideCount-1; i>=0; i--){
+        const cloneSlide = slide[i].cloneNode(true)
+        cloneSlide.classList.add = 'clone'
+        slides.prepend(cloneSlide)
+    }
+    updateWidth()
+    setInitaulpos()
+
+    setTimeout(()=>{
+        slides.classList.add('animated')
+    },100)
+    
+
 }
 
-showSliding()
+const updateWidth = () => {
+    const   currentSlides = document.querySelectorAll('#slides > li')
+            newSlideCount = currentSlides.length
+            newWidth = `${(slideWidth + slideMargin) * newSlideCount - slideMargin}px`
+
+    slides.style.width = newWidth
+}
+
+const setInitaulpos = () => {
+    const TranslateValue = -(slideWidth + slideMargin)* slideCount
+    slides.style.transform = `translateX(${TranslateValue}px)`
+}
+
+nextBtn.addEventListener('click', ()=>{
+    moveSlide(currentIndex+1)
+    stopSlide()
+    autoSlide()
+})
+prevBtn.addEventListener('click', ()=>{
+    moveSlide(currentIndex-1)
+    stopSlide()
+    autoSlide()
+})
+
+const moveSlide = (num) => {
+    slides.style.left = `${-(slideWidth + slideMargin) * num}px`
+    currentIndex = num
+    
+    if ( currentIndex === slideCount || currentIndex === -slideCount){
+        setTimeout(()=>{
+            slides.classList.remove('animated')
+            slides.style.left = '0px'
+            currentIndex = 0
+            
+        },600)
+
+    }
+    slides.classList.add('animated')
+}
+
+
+
+const autoSlide = () => {
+    timer = setInterval(()=>{
+        moveSlide(currentIndex+1)
+    }, 2000)
+}
+
+const stopSlide = () => {
+    clearInterval(timer)
+}
+
+makeClone()
+autoSlide()
 
 
 const targets_l = document.querySelectorAll(".fade-class-left")
@@ -84,28 +97,44 @@ const observer_l = new IntersectionObserver(function (entries, observer) {
     entries.forEach((entry) => {
         const container = entry.target;
         if (entry.isIntersecting) {
-            container.classList.add("fade-in-left");
+            container.classList.add("fade-in-left")
         } else {
-            container.classList.remove("fade-in-left");
+            container.classList.remove("fade-in-left")
         }
     });
 }, options)
 
 targets_l.forEach((target) => {
-    observer_l.observe(target);
+    observer_l.observe(target)
 })
 
 const observer_r = new IntersectionObserver(function (entries, observer) {
     entries.forEach((entry) => {
-        const container = entry.target;
+        const container = entry.target
         if (entry.isIntersecting) {
-            container.classList.add("fade-in-right");
+            container.classList.add("fade-in-right")
         } else {
-            container.classList.remove("fade-in-right");
+            container.classList.remove("fade-in-right")
         }
-    });
+    })
 }, options)
 
 targets_r.forEach((target) => {
-    observer_r.observe(target);
+    observer_r.observe(target)
 })
+
+//---------------------------------------
+
+const community = document.querySelector('#community')
+
+
+const commuHandler = (e) => {
+    cookie = document.cookie.split('=')[1] ===''
+    if (cookie) {
+        alert('로그인이 필요한 컨텐츠입니다.')
+        e.preventDefault();
+        return
+    }
+}
+
+community.addEventListener('click',commuHandler)
--- repository/boardRepository.js
+++ repository/boardRepository.js
@@ -27,14 +27,14 @@ exports.viewAll = async ()=>{
 
 
 exports.modifyOne = async(idx) =>{
-    const sql = `SELECT * FROM boardWrite WHERE idx=${idx}`
+    const sql = `SELECT * FROM board WHERE idx=${idx}`
     const [result] = await db.query(sql)
     return result
 }
 
 exports.modifyPost = async(modifyInfo) => {
     const payload = Object.entries(modifyInfo).map(([k,v]) => `"${v}"`).join(",")
-    const sql = `UPDATE boardWrite SET subject ='${modifyInfo.subject}', content ='${modifyInfo.content}' WHERE idx=${modifyInfo.idx}`
+    const sql = `UPDATE board SET subject ='${modifyInfo.subject}', content ='${modifyInfo.content}' WHERE idx=${modifyInfo.idx}`
     const [result] = await db.query(sql)
     return result
 }
@@ -54,11 +54,5 @@ exports.hitCount = async({idx,hit})=>{
     db.query(sql)
 }
 
-const writeInfo = {
-    nickname : '우기',
-    subject : 'ㅎㅇ',
-    content : 'ㅎㅇ'
-}
 
-this.findAll({writeInfo})
 
