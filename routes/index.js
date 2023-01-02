671f992329c922ca7f5b106b0760695150c4662f
jungmin <justin930522@gmail.com>
2023-01-02 21:07:48 +0900

    feat:token없을때index랜더링오류수정

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const router = express.Router()
 
+
 const user = require('./user.route')
 const board = require('./board/boardRoute')
 const admin = require('./admin.route')
 
-router.get('/',(req,res)=>{
-    const cookies = req.cookies.token === '' ? undefined : JSON.parse(req.cookies.token).nickname
-    res.render('index.html', {token:cookies})
+
+router.get('/', (req,res)=>{
+    // if(res.cookies.token !== undefined){
+    //     const cookies = req.cookies.token !== '' ? JSON.parse(req.cookies.token).nicknamed : undefined
+    //     console.log(cookies)
+    //     res.render('index.html', {token:cookies})
+    // } else{
+    //     res.render("index.html")
+    // }
+    const {token} = req.cookies
+    console.log(token)
+    res.render('index.html',{token})
 })
 
 router.use('/user', user)
