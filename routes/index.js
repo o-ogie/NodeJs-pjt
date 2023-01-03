d1c7fe80ffff635d8c342d28fcd78161bbb9ed02
o-ogie <wrld.worthy@gmail.com>
2023-01-03 18:28:34 +0900

    fix:indexpage json형식 오류 해결

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,11 @@ const board = require('./board/boardRoute')
 const admin = require('./admin.route')
 
 router.get('/',(req,res)=>{
-    const cookies = req.cookies.token === '' ? undefined : JSON.parse(req.cookies.token).nickname
-    res.render('index.html', {token:cookies})
+
+    let cookies = Object.values(req.cookies).toString()
+    cookies = cookies === '' ? undefined : JSON.parse(cookies).nickname
+
+    res.render('index.html',{token: cookies})
 })
 
 router.use('/user', user)
